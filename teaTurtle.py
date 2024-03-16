080a69bfc1b3f9ee9640cd33e5887dcdee22fef1
raf <73646305+rafsandwich@users.noreply.github.com>
2024-03-16 21:54:30 +0000

    msg delete handling
    looping errors still exist

--- teaTurtle.py
+++ teaTurtle.py
@@ -3,27 +3,48 @@ import asyncio
 from discord.ext import commands
 
 # set bot token
-TOKEN = '###'
+TOKEN = '1'
+
+# set channel id
+cID = 1
+
+# store ID of last sent reminder message (for deletion)
+last_message_id = None
+
+intents = discord.Intents.default()
+intents.messages = True
 
 # init bot
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 # on ready
-@bot.event
-async def on_ready():
-    print(f'{bot.user} is logged in and ready')
-
-# hydrate prompt
 async def water_reminder():
+    global last_message_id
     await bot.wait_until_ready()
-    channel = bot.get_channel(your_channel_id)  # server channel id
+    channel = bot.get_channel(cID)
 
     while not bot.is_closed():
-        await channel.send("It's time to drink water!")
+        if last_message_id:
+            # delete previous reminder
+            try:
+                last_message = await channel.fetch_message(last_message_id)
+                await last_message.delete()
+            except discord.NotFound:
+                pass  # no msg, do nothing
+
+        # send new reminder msg
+        new_message = await channel.send("It's time to drink water!")
+        last_message_id = new_message.id
+
         await asyncio.sleep(3600)  # 1 hour
 
-# start reminder loop
-bot.loop.create_task(water_reminder())
+# asynchronous main function
+async def main():
+    # start reminder loop
+    bot.loop.create_task(water_reminder())
 
-# run bot
-bot.run(TOKEN)
+    # run bot
+    await bot.start(TOKEN)
+
+# run main func
+asyncio.run(main())

6f38b5f1fca1ca8103fc0f468461f8162b281d9f
raf <73646305+rafsandwich@users.noreply.github.com>
2024-03-16 23:16:16 +0000

    smarter time logic
    experiments with forcing the script to wait for the next hour (e.g. 17:00, 18:00) and to then run on a given interval (e.g. 5 mins, 30 mins, hourly)

--- teaTurtle.py
+++ teaTurtle.py
@@ -1,6 +1,7 @@
 import discord
 import asyncio
 from discord.ext import commands
+import datetime
 
 # set bot token
 TOKEN = '1'
@@ -12,10 +13,42 @@ cID = 1
 lastMsgID = None
 
 intents = discord.Intents.default()
+intents.message_content = True
 intents.messages = True
 
 # init bot
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+# calc time to next hour
+# def calc_next_hour():
+#     now = datetime.datetime.now()
+#     nextHour = now.replace(hour=now.hour + 1, minute=0, second=0, microsecond=0)
+#     return (nextHour - now).total_seconds()
+
+def calc_next_hour():
+    now = datetime.datetime.now()
+    nextHour = now.replace(hour=(now.hour + 1) % 24, minute=0, second=0, microsecond=0)
+    timeToNextHour = (nextHour - now).total_seconds()
+    return timeToNextHour # handle wrapping around if function is started at 23:00
+
+
+
+
+# calculate next reminder time
+# def calc_next_reminder():
+#     now = datetime.datetime.now()
+#     return now + datetime.timedelta(minutes=(5 - now.minute % 5)) # every 5 mins 
+
+# def calc_next_reminder():
+#     now = datetime.datetime.now()
+#     return now.replace(hour=now.hour + 1, minute=0, second=0, microsecond=0) # every hour
+
+def calc_next_reminder():
+    now = datetime.datetime.now()
+    if now.minute < 30:
+        return now.replace(minute=30, second=0, microsecond=0)
+    else:
+        return now.replace(hour=now.hour + 1, minute=0, second=0, microsecond=0) # every half hour
 
 # send reminder
 async def water_reminder():
@@ -24,19 +57,34 @@ async def water_reminder():
     channel = bot.get_channel(cID)
 
     while not bot.is_closed():
-        if lastMsgID:
-            # delete previous reminder
-            try:
-                last_message = await channel.fetch_message(lastMsgID)
-                await last_message.delete()
-            except discord.NotFound:
-                pass  # no msg, do nothing
+        # calc time until first reminder
+        nextReminder = calc_next_hour()
 
-        # send new reminder msg
-        new_message = await channel.send("~ ding ding tea time ~")
-        lastMsgID = new_message.id
+        # wait until init reminder
+        await asyncio.sleep(nextReminder)
+        
+        # start sending every 5 mins
+        while True:
+            # calc time until next reminder
+            nextReminderTime = calc_next_reminder()
+            nextReminder = (nextReminderTime - datetime.datetime.now()).total_seconds()
 
-        await asyncio.sleep(36)  # 1 hour
+            # wait until next reminder
+            await asyncio.sleep(nextReminder)
+        
+            if lastMsgID:
+                # try delete previous reminder
+                try:
+                    last_message = await channel.fetch_message(lastMsgID)
+                    await last_message.delete()
+                except discord.NotFound:
+                    pass  # no msg, do nothing
+
+            # send new reminder msg
+            new_message = await channel.send("~ ding ding tea time ~")
+            lastMsgID = new_message.id
+
+            #await asyncio.sleep(3600)  # 1 hour || redundant if using next reminder calculator
 
 @bot.event
 async def on_ready():
